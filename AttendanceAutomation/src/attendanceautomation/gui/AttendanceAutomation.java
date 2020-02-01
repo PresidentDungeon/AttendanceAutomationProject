db54f661ba6deba96c52ed4ed0905d390a300c33
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-02-01 16:47:24 +0100

    Added Login Page

--- AttendanceAutomation/src/attendanceautomation/gui/AttendanceAutomation.java
+++ AttendanceAutomation/src/attendanceautomation/gui/AttendanceAutomation.java
@@ -20,7 +20,7 @@ public class AttendanceAutomation extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/AttendanceView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/LoginView.fxml"));
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
