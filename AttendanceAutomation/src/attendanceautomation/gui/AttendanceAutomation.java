021008afd3365d8d52cf1ac92eb1e89694e60ff2
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-02-01 14:35:30 +0100

    Initial Commit

---
+++ AttendanceAutomation/src/attendanceautomation/gui/AttendanceAutomation.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendanceautomation.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Peter
+ */
+public class AttendanceAutomation extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/AttendanceView.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        stage.getIcons().add(new Image(AttendanceAutomation.class.getResourceAsStream("images/imageedit_1_3887033642.png")));
+        stage.setTitle("EASV Attendance");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ AttendanceAutomation/src/attendanceautomation/gui/controllers/AttendanceController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendanceautomation.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author ander
+ */
+public class AttendanceController {
+
+    @FXML
+    private TableView<?> overviewTable;
+    @FXML
+    private ComboBox<?> comboSort;
+    
+}
