ac21805fe538fa24e8d1c0dfef9eec4e00f32ae6
pjieder <55383141+pjieder@users.noreply.github.com>
2020-02-19 14:15:54 +0100

    username = 1 | password = 2

--- AttendanceAutomation/src/attendanceautomation/gui/controllers/LoginController.java
+++ AttendanceAutomation/src/attendanceautomation/gui/controllers/LoginController.java
@@ -11,6 +11,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -21,10 +23,21 @@ import javafx.stage.Stage;
 public class LoginController {
 
     AppModel appModel = new AppModel();
+    
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    
+    
+    
     @FXML
     private void login(MouseEvent event) throws IOException, InterruptedException {
         
             //Should later check if username and password is correct.
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+            if(username.equals("1")&&password.equals("2")) {
             
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(AppModel.class.getResource("views/AttendanceView.fxml"));
@@ -35,7 +48,8 @@ public class LoginController {
             Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             appStage.setScene(scene);
             appStage.show();
-        
+            }else
+                System.out.println("WRONG! Username = 1 | Password = 2");
         
         
     }
