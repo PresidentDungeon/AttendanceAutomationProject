1169788ada5ea59c37894b4792f93ba6faa9939b
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-03-12 14:28:59 +0100

    Added working remember me button
    Added functionality for rembembering username and password if selected.

--- AttendanceAutomation/src/attendanceautomation/gui/controllers/LoginViewController.java
+++ AttendanceAutomation/src/attendanceautomation/gui/controllers/LoginViewController.java
@@ -10,39 +10,76 @@ import attendanceautomation.be.Roles;
 import attendanceautomation.be.Student;
 import attendanceautomation.be.Teacher;
 import attendanceautomation.gui.AppModel;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
 /**
  *
  * @author ander
  */
-public class LoginViewController {
+public class LoginViewController implements Initializable {
 
     AppModel appModel = new AppModel();
+
+    Preferences preferences;
 
     @FXML
     private JFXTextField usernameField;
     @FXML
     private JFXPasswordField passwordField;
+    @FXML
+    private JFXCheckBox rememberMe;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        preferences = Preferences.userRoot().node("AttendanceAutomation");
+        if (preferences != null)
+        {
+            usernameField.setText(preferences.get("username", null));
+            passwordField.setText(preferences.get("password", null));
+            rememberMe.setSelected(preferences.getBoolean("rememberActivated", false));
+        }
+        
+        
+    }
 
     @FXML
     private void login(MouseEvent event) throws IOException, InterruptedException {
         String username = usernameField.getText();
         String password = passwordField.getText();
-        
+
         Person personToValidate = appModel.validateUser(username, password);
 
         if (personToValidate == null) {
-        // some kind of error message should show here?
-            
+            // some kind of error message should show here?
+
         } else {
+
+            if (rememberMe.isSelected())
+            {
+                preferences.put("username", usernameField.getText());
+                preferences.put("password", passwordField.getText());
+                preferences.putBoolean("rememberActivated", true);
+            }
+            else
+            {
+                preferences.put("username", "");
+                preferences.put("password", "");
+                preferences.putBoolean("rememberActivated", false);
+            }
 
             FXMLLoader fxmlLoader = new FXMLLoader();
             Scene scene = null;
